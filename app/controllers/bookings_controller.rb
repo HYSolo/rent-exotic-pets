35e36219636870b1a30113ede6f14c4e0aeb1b77
Shankar Isaac <khaarn.s@gmail.com>
2021-06-08 09:44:46 +1000

    testing seeds.rb

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,7 @@
 class BookingsController < ApplicationController
   def new
-    @booking = Booking.new
   end
 
   def create
-    @booking = Booking.new(booking_param)
-  end
-
-  private
-
-  def booking_param
-    params.require(:booking).permit(:start_time, :end_date, :total_price)
   end
 end
