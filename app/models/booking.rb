d18a4c94bdc955c26ae05f445ea69772673383ef
Matt Baddour <mattbaddour@gmail.com>
2021-06-08 16:44:52 +1000

    added date validation to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,11 @@ class Booking < ApplicationRecord
   belongs_to :pet
 
   validates :start_date, :end_date, :total_price, presence: true
+  validate :end_date_cannot_be_before_start_date
+
+  def end_date_cannot_be_before_start_date
+    if :start_date.present? &&  :end_date <= :start_date
+      errors.add(:end_date, "can't be before start date")
+    end
+  end
 end
