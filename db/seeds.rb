f24cfbdc954c5b0230e6da09a280f0a3cacbde90
Hillel Solomon <solomonhillel@gmail.com>
2021-06-08 15:12:53 +1000

    finished seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 User.delete_all
 Pet.delete_all
 
+# Creating 10 users
 10.times do
   name = Faker::Name.name
   email = Faker::Internet.email
@@ -16,13 +17,37 @@ Pet.delete_all
   User.create!(name: name, email: email, password: "secret", location: location)
 end
 
-50.times do
+# Creating 20 pets that don't have any bookings
+20.times do
   name = Faker::GreekPhilosophers.name
   description = Faker::Coffee.notes
   species = ['Alligator', "Tiger", "Lion", "Cheetah", "Bear"].sample
   danger_meter = rand(1..5)
   price = rand(100..1000)
   location = Faker::Address.city
-  user = User.find(rand(1..10))
+  user = User.all.sample
   pet = Pet.create!(name: name, description: description, species: species, danger_meter: danger_meter, price: price, location: location, user: user)
+end
+
+#Creating 20 pets that *do* have bookings
+20.times do
+  name = Faker::GreekPhilosophers.name
+  description = Faker::Coffee.notes
+  species = ['Alligator', "Tiger", "Lion", "Cheetah", "Bear"].sample
+  danger_meter = rand(1..5)
+  price = rand(100..1000)
+  location = Faker::Address.city
+  user = User.all.sample
+  pet = Pet.create!(name: name, description: description, species: species, danger_meter: danger_meter, price: price, location: location, user: user)
+  
+  start_date = Faker::Date.in_date_period(year: 2021, month: 6)
+  weeks = rand(1..10)
+  end_date = start_date + (7 * weeks)
+  total_price = price * weeks
+  if user == User.first
+    new_user = User.last
+  else
+    new_user = User.find(user.id - 1)
+  end
+  booking = Booking.create!(start_date: start_date, end_date: end_date, total_price: total_price, user: new_user, pet: pet)
 end
