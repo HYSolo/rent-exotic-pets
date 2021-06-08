3a9b2fde785fadc75e65879c57bf5b36736bd572
Shankar Isaac <khaarn.s@gmail.com>
2021-06-08 11:56:13 +1000

    updated bookings controller & view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,14 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_param)
+
+  private
+
+  def booking_param
+    params.require(:booking).permit(:start_time, :end_date, :total_price)
   end
 end
