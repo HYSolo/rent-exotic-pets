e47c75af8585ba6a8db3fd5b18e1cc110c55f139
Matt Baddour <mattbaddour@gmail.com>
2021-06-08 15:19:16 +1000

    bookings create init

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,16 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @pet = Pet.new(params[:id])
   end
 
   def create
     @booking = Booking.new(booking_param)
+  end
 
   private
 
   def booking_param
-    params.require(:booking).permit(:start_time, :end_date, :total_price)
+    params.require(:booking).permit(:start_time, :end_date, :total_price, :pet_id)
   end
 end
