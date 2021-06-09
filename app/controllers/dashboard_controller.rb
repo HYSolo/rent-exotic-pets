47c5026bd2428935194de765b3d457ebeaf1e546
Matt Baddour <mattbaddour@gmail.com>
2021-06-09 15:36:05 +1000

    updated dashboard code to be cleaner

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @pets = current_user.pets.select { |pet| pet.bookings.exists? }
-    @bookings = @pets.map { |pet| pet.bookings }
-    # byebug
+    @pets = current_user.pets
+    @booked_pets = current_user.pets.select { |pet| pet.bookings.exists? }
   end
 end
